37093a64aa427f05d50b0ea6fdabc3e1acc9a1d2
TieStone <416138794@qq.com>
2023-08-30 15:38:50 +0800

    revome * on both sides of title

--- Markdown Item URI.js
+++ Markdown Item URI.js
@@ -1,38 +1,42 @@
 {
-	"translatorID":"8dbf9b92-f796-4153-8f2e-ac6c157500dc",
-	"translatorType":2,
-	"label":"Markdown Item URI",
-	"creator":"Silent",
-	"target":"markdown",
-	"minVersion":"2.0",
-	"maxVersion":"",
-	"priority":200,
-	"inRepository":false,
-	"lastUpdated":"2020-11-30"
-	}
-	 
-	
-	function doExport() {
-		var item;
-		while(item = Zotero.nextItem()) {
-			var date = Zotero.Utilities.strToDate(item.date).year;
+  translatorID: "8dbf9b92-f796-4153-8f2e-ac6c157500dc",
+  translatorType: 2,
+  label: "Markdown Item URI",
+  creator: "Silent",
+  target: "markdown",
+  minVersion: "2.0",
+  maxVersion: "",
+  priority: 200,
+  inRepository: false,
+  lastUpdated: "2020-11-30",
+};
 
-			var year = date && !isNaN(date) ? date + ". " : (typeof item.date == 'undefined'?  "" : item.date + ". ");
-			var library_id = item.libraryID ? item.libraryID : "";
-			var author_lastname = getValidAuthor(item);
-			var title = item.title ? "*" + item.title + "*" : "";
-			var key = item.key;
-	
-			Zotero.write(`[${author_lastname}${year}${title}](zotero://select/items/${library_id}_${key})`);
-		}
-	}
+function doExport() {
+  var item;
+  while ((item = Zotero.nextItem())) {
+    var date = Zotero.Utilities.strToDate(item.date).year;
 
-	function getValidAuthor(item){
-		if(item.creators && item.creators[0] && item.creators[0].lastName){
-			return item.creators[0].lastName + ". ";
-		}else{
-			return "";
-		}
+    var year =
+      date && !isNaN(date)
+        ? date + ". "
+        : typeof item.date == "undefined"
+        ? ""
+        : item.date + ". ";
+    var library_id = item.libraryID ? item.libraryID : "";
+    var author_lastname = getValidAuthor(item);
+    var title = item.title ? " " + item.title + " " : "";
+    var key = item.key;
 
-	}
-	
+    Zotero.write(
+      `[${author_lastname}${year}${title}](zotero://select/items/${library_id}_${key})`
+    );
+  }
+}
+
+function getValidAuthor(item) {
+  if (item.creators && item.creators[0] && item.creators[0].lastName) {
+    return item.creators[0].lastName + ". ";
+  } else {
+    return "";
+  }
+}
